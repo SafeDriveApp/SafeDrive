40542ae2b2b8f481d6a482423f1d2d027083bde0
tektung <2241720128@student.polinema.ac.id>
2024-10-23 21:43:15 +0700

    fix: signup page

---
+++ lib/Page/signup_page.dart
@@ -0,0 +1,151 @@
+import 'package:flutter/material.dart';
+
+class SignupPage extends StatefulWidget {
+  const SignupPage({super.key});
+
+  @override
+  State<SignupPage> createState() => _SignupPageState();
+}
+
+class _SignupPageState extends State<SignupPage> {
+  @override
+  Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
+    return Scaffold(
+      backgroundColor: Color.fromRGBO(255, 255, 255, 1),
+      body: SingleChildScrollView(
+        // Added SingleChildScrollView
+        child: Padding(
+          padding: const EdgeInsets.all(1),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              Center(
+                child: Padding(
+                  padding: EdgeInsets.fromLTRB(0, screenWidth * 20 / 100, 0, 0),
+                  child: Column(
+                    mainAxisSize: MainAxisSize.min,
+                    children: [
+                      Container(
+                        width: 200, // Increased width
+                        height: 200, // Increased height
+                        child: const Image(
+                          image: AssetImage("assets/logo.png"),
+                          fit: BoxFit.fill,
+                        ),
+                      ),
+                      SizedBox(height: 10), // Space between image and text
+                      Text(
+                        'Create an account',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18, // Adjust font size as needed
+                          color: Color(0xFF272343), // Color in hex
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: screenHeight *
+                    5 /
+                    100, // Reduced space between text and form
+              ),
+              Padding(
+                padding: EdgeInsets.symmetric(
+                    horizontal: screenWidth *
+                        5 /
+                        100), // Added padding to left and right
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    Text("Nama"),
+                    SizedBox(
+                      height: 2,
+                    ),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'odin',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text("Email Address"),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Odin@gmail.com',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text("Password"),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Password',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Text("Confirm Password"),
+                    TextField(
+                      decoration: InputDecoration(
+                        border: OutlineInputBorder(),
+                        hintText: 'Confirm Password',
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Center(
+                      child: ElevatedButton(
+                        onPressed: () {
+                          // Handle sign up action
+                        },
+                        style: ElevatedButton.styleFrom(
+                          backgroundColor: Color(0xFFFFD803), // Button color
+                          padding: EdgeInsets.symmetric(
+                              horizontal: 50, vertical: 15),
+                          textStyle: TextStyle(
+                            fontFamily: 'Poppins',
+                            fontWeight: FontWeight.w600, // Semi-bold
+                            fontSize: 16,
+                          ),
+                        ),
+                        child: Text('SIGN UP'),
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Center(
+                      child: Text(
+                        'Have an account already? Log In',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.normal,
+                          fontSize: 14,
+                          color: Color(0xFF272343),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
