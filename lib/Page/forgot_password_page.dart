b02239bcb36218c97b93fb1496cddcbed288bb46
tektung <2241720128@student.polinema.ac.id>
2024-10-24 08:54:39 +0700

    add : forgot password page

--- lib/Page/forgot_password_page.dart
+++ lib/Page/forgot_password_page.dart
@@ -1,18 +1,99 @@
 import 'package:flutter/material.dart';
+import 'package:safe_drive/Page/login_page.dart';
 
-class ForgotPasswordPage extends StatelessWidget {
+class ForgotPasswordPage extends StatefulWidget {
   const ForgotPasswordPage({super.key});
 
   @override
+  State<ForgotPasswordPage> createState() => _ForgotPasswordPageState();
+}
+
+class _ForgotPasswordPageState extends State<ForgotPasswordPage> {
+  final TextEditingController _emailController = TextEditingController();
+
+  @override
   Widget build(BuildContext context) {
+    double screenWidth = MediaQuery.of(context).size.width;
+    double screenHeight = MediaQuery.of(context).size.height;
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Forgot Password'),
-      ),
-      body: Center(
-        child: Text(
-          'Forgot Password Page',
-          style: Theme.of(context).textTheme.headlineSmall,
+      backgroundColor: Color.fromRGBO(255, 255, 255, 1),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsets.symmetric(horizontal: screenWidth * 5 / 100),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              SizedBox(height: screenHeight * 10 / 100),
+              Row(
+                children: [
+                  IconButton(
+                    icon: Icon(Icons.arrow_back),
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                            builder: (context) => const LoginPage()),
+                      ); // Navigate back to the previous page
+                    },
+                  ),
+                  Expanded(
+                    child: Center(
+                      child: Text(
+                        'Forgot Password',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 24,
+                          color: Color(0xFF272343),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+              SizedBox(height: screenHeight * 5 / 100),
+              Text(
+                'Enter your email address to recover your password:',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 16,
+                  color: Color(0xFF272343),
+                ),
+              ),
+              SizedBox(height: 20),
+              TextField(
+                controller: _emailController,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter your email',
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 20),
+              Center(
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Handle password recovery
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFFFFD803), // Button color
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      textStyle: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w600, // Semi-bold
+                        fontSize: 16,
+                        color: Colors.black, // Text color
+                      ),
+                    ),
+                    child: Text('RECOVER PASSWORD'),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
