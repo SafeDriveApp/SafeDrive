4ee15e038d40f0d480e6ecfbc38163c0d1b9ae77
tektung <2241720128@student.polinema.ac.id>
2024-10-24 08:55:06 +0700

    fix : new logic

--- lib/Page/login_page.dart
+++ lib/Page/login_page.dart
@@ -1,5 +1,7 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:safe_drive/Page/forgot_password_page.dart';
+import 'package:safe_drive/Page/signup_page.dart'; // Import the ForgotPasswordPage
 
 class LoginPage extends StatefulWidget {
   const LoginPage({super.key});
@@ -9,88 +11,154 @@ class LoginPage extends StatefulWidget {
 }
 
 class _LoginPageState extends State<LoginPage> {
+  bool _isPasswordVisible = false;
+
   @override
   Widget build(BuildContext context) {
     double screenWidth = MediaQuery.of(context).size.width;
     double screenHeight = MediaQuery.of(context).size.height;
-    
     return Scaffold(
       backgroundColor: const Color.fromRGBO(255, 255, 255, 1),
-      body: SizedBox(
-        width: screenWidth,
-        height: screenHeight,
+      body: SingleChildScrollView(
         child: Padding(
-          padding: EdgeInsets.fromLTRB(screenWidth * 2 / 100, 20, 20, 0),
+          padding: EdgeInsets.symmetric(horizontal: screenWidth * 5 / 100),
           child: Column(
             crossAxisAlignment: CrossAxisAlignment.start,
-            mainAxisAlignment: MainAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              SizedBox(height: screenHeight * 10 / 100),
               Center(
-                child: Padding(
-                  padding: EdgeInsets.fromLTRB(0, screenWidth * 20 / 100, 0, 0),
-                  child: const SizedBox(
-                    width: 100,
-                    height: 100,
-                    child: Image(
-                      image: AssetImage("assets/img/logo.png"),
-                      fit: BoxFit.fill,
+                child: Text(
+                  'Login',
+                  style: TextStyle(
+                    fontFamily: 'Poppins',
+                    fontWeight: FontWeight.bold,
+                    fontSize: 24,
+                    color: Color(0xFF272343),
+                  ),
+                ),
+              ),
+              SizedBox(height: screenHeight * 5 / 100),
+              Text(
+                'Email Address',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 16,
+                  color: Color(0xFF272343),
+                ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter your email',
+                ),
+                keyboardType: TextInputType.emailAddress,
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Password',
+                style: TextStyle(
+                  fontFamily: 'Poppins',
+                  fontSize: 16,
+                  color: Color(0xFF272343),
+                ),
+              ),
+              SizedBox(height: 10),
+              TextField(
+                obscureText: !_isPasswordVisible,
+                decoration: InputDecoration(
+                  border: OutlineInputBorder(),
+                  hintText: 'Enter your password',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      _isPasswordVisible
+                          ? Icons.visibility
+                          : Icons.visibility_off,
+                    ),
+                    onPressed: () {
+                      setState(() {
+                        _isPasswordVisible = !_isPasswordVisible;
+                      });
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(height: 20),
+              Align(
+                alignment: Alignment.centerRight,
+                child: TextButton(
+                  onPressed: () {
+                    // Navigate to ForgotPasswordPage
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ForgotPasswordPage()),
+                    );
+                  },
+                  child: Text(
+                    'Forgot Password?',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      color: Colors.blue,
                     ),
                   ),
                 ),
               ),
-              SizedBox(
-                height: screenHeight * 20 / 100,
+              SizedBox(height: 20),
+              Center(
+                child: SizedBox(
+                  width: double.infinity,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Handle login action
+                    },
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Color(0xFFFFD803), // Button color
+                      padding: EdgeInsets.symmetric(vertical: 15),
+                      textStyle: TextStyle(
+                        fontFamily: 'Poppins',
+                        fontWeight: FontWeight.w600, // Semi-bold
+                        fontSize: 16,
+                        color: Colors.black, // Text color
+                      ),
+                    ),
+                    child: Text('LOGIN'),
+                  ),
+                ),
               ),
-              Padding(
-                padding: EdgeInsets.fromLTRB(0, screenWidth * 2 / 100, 0, 0),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const Text("Name"),
-                    const SizedBox(height: 2),
-                    const TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'odin',
-                      ),
+              SizedBox(height: 20),
+              Center(
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Don\'t have an account? ',
+                    style: TextStyle(
+                      fontFamily: 'Poppins',
+                      fontWeight: FontWeight.normal,
+                      fontSize: 14,
+                      color: Color(0xFF272343),
                     ),
-                    const SizedBox(height: 20),
-                    const Text("Email Address"),
-                    const TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Odin@gmail.com',
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    const Text("Password"),
-                    const TextField(
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(),
-                        hintText: 'Password',
-                      ),
-                    ),
-                    const SizedBox(height: 20),
-                    
-                    // Tambahkan tombol Forgot Password
-                    Align(
-                      alignment: Alignment.centerRight,
-                      child: TextButton(
-                        onPressed: () {
-                          // Navigasi ke halaman Forgot Password
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(builder: (context) => const ForgotPasswordPage()),
-                          );
-                        },
-                        child: const Text(
-                          "Forgot Password?",
-                          style: TextStyle(color: Colors.blue),
+                    children: [
+                      TextSpan(
+                        text: 'Sign Up',
+                        style: TextStyle(
+                          fontFamily: 'Poppins',
+                          fontWeight: FontWeight.bold,
+                          fontSize: 14,
+                          color: Color(0xFF272343),
                         ),
+                        recognizer: TapGestureRecognizer()
+                          ..onTap = () {
+                            // Navigate to SignupPage
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) => SignupPage()),
+                            );
+                          },
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ],
