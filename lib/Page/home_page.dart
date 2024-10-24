d9454151385212e0c78c6c02c00ee8f314135d6e
tektung <2241720128@student.polinema.ac.id>
2024-10-24 09:50:10 +0700

    add : AppBar

--- lib/Page/home_page.dart
+++ lib/Page/home_page.dart
@@ -28,6 +28,9 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text('Home'),
+      ),
       body: Center(
         child: _widgetOptions.elementAt(_selectedIndex),
       ),
